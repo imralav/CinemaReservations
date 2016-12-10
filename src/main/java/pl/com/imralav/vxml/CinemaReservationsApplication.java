f2b63885806227432921e58809d4e511c6762eaa
Tomasz Karczewski <tomasz.karczewski@contractors.roche.com>
2016-12-10 09:30:32 +0100

    Updated git ignore. Added initial gradle stuff from Spring Initializr

---
+++ src/main/java/pl/com/imralav/vxml/CinemaReservationsApplication.java
@@ -0,0 +1,12 @@
+package pl.com.imralav.vxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaReservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaReservationsApplication.class, args);
+	}
+}
