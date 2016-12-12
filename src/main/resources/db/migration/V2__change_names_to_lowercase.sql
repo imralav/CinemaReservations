cd0782fa319ecfae4033aa18113707dd31e975d6
Tomasz Karczewski <tomasz.karczewski@contractors.roche.com>
2016-12-12 19:08:08 +0100

    Added new database version to rename tables

---
+++ src/main/resources/db/migration/V2__change_names_to_lowercase.sql
@@ -0,0 +1,11 @@
+rename table CUSTOMER to customer;
+
+rename table GENRE to genre;
+
+rename table MOVIE to movie;
+
+rename table SEAT to seat;
+
+rename table SHOWING to showing;
+
+rename table BOOKING to booking;
