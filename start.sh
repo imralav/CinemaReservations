d6ce0a757ca0152de8da10b01d7c65144bf6a822
Tomasz Karczewski <tomasz.karczewski@contractors.roche.com>
2016-12-14 11:57:06 +0100

    Added start.sh to kill old server, copy jar and run new server

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+DEPLOY_FOLDER="$CINEMA_HOME/deploy"
+if ! [ -d "$DEPLOY_FOLDER" ]; then
+	echo "Creating $DEPLOY_FOLDER"
+	mkdir -p "$DEPLOY_FOLDER"
+fi
+if [ -e "$DEPLOY_FOLDER/pid" ]; then
+	kill -9 `cat "$DEPLOY_FOLDER/pid"`
+fi
+echo "Removing old deploy files"
+rm $DEPLOY_FOLDER/cinema.jar
+cp "build/libs/cinema.jar" "$DEPLOY_FOLDER"
+java -jar build/libs/*.jar &
+echo $! > $DEPLOY_FOLDER/pid
