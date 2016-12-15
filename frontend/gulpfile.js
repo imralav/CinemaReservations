8683201fb0cc673c14228b926a35fc6237af2dd9
Tomasz Karczewski <tomasz.karczewski@contractors.roche.com>
2016-12-15 07:55:57 +0100

    JavaScript files are now minified. Build script is now simplified on the frontend side.

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -4,21 +4,32 @@ var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var Server = require('karma').Server;
 var runSequence = require('run-sequence');
+var concat = require('gulp-concat');  
+var rename = require('gulp-rename');  
+var uglify = require('gulp-uglify'); 
 
 var publicFolderPath = '../src/main/resources/public';
+var jsFiles = 'js/**/*.js';
+var jsDest = 'build';
 
-gulp.task('copy', function() {
-	return gulp.src("js/**/*").pipe(gulp.dest(publicFolderPath));
+gulp.task('build:js', function() {  
+	var paths = mainBowerFiles();
+	paths.push(jsFiles);
+	return gulp.src(paths)
+	    .pipe(concat('scripts.js'))
+	    .pipe(rename('scripts.min.js'))
+	    .pipe(uglify())
+	    .pipe(gulp.dest(jsDest));
 });
 
-gulp.task('copyBowerDependencies', function() {
-	return gulp.src(mainBowerFiles(), {
-		base : 'bower_components'
-	}).pipe(gulp.dest(publicFolderPath));
+gulp.task('build', ['build:js']);
+
+gulp.task('copy', function() {
+	return gulp.src(jsDest + '/**/*.js').pipe(gulp.dest(publicFolderPath + '/js'));
 });
 
 gulp.task('lint', function() {
-	return gulp.src('js/**/*')
+	return gulp.src(jsFiles)
 			.pipe(jshint())
 			.pipe(jshint.reporter('jshint-stylish'))
 			.pipe(jshint.reporter('fail'));
@@ -41,7 +52,7 @@ gulp.task('test', function(done) {
 
 gulp.task('watch', function(done) {
 	var watchOptions = { debounceDelay: 1000 };
-	gulp.watch('js/**/*.js', watchOptions, ['lint', 'test']);
+	gulp.watch(jsFiles, watchOptions, ['lint', 'test']);
 	gulp.watch('test/**/*.js', watchOptions, ['test']);
 });
 
