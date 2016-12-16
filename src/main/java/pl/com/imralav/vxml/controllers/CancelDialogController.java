5367d89909da383ffb4847fa8f03a4290af815f1
Tomasz Karczewski <tomasz.karczewski@contractors.roche.com>
2016-12-16 18:39:18 +0100

    Expanded on cancel branch of dialog. Logs should now be saved to logs/cinema.log

--- src/main/java/pl/com/imralav/vxml/controllers/CancelDialogController.java
+++ src/main/java/pl/com/imralav/vxml/controllers/CancelDialogController.java
@@ -1,13 +1,26 @@
 package pl.com.imralav.vxml.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/cancel")
 public class CancelDialogController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CancelDialogController.class);
 
-    @RequestMapping("/cancel")
+    @RequestMapping
     public String mainDialog() {
         return "cancel/mainCancel";
+    }
+
+    @RequestMapping("/passCustomerCode")
+    public String passCustomerCode(HttpServletRequest request) {
+        LOGGER.info("Request attributes passed: {}", request.getAttributeNames());
+        LOGGER.info("Request params passed: {}", request.getParameterMap());
+        return "cancel/customerUnknown";
     }
 }
