acb075e66de18aaa7f6b95dad9f06a808faee36b
Tomasz Karczewski <tomasz.karczewski@contractors.roche.com>
2017-01-02 09:14:13 +0100

    Issue#18 fixed some warnings

--- src/main/java/pl/com/imralav/vxml/controllers/ReservationDialogController.java
+++ src/main/java/pl/com/imralav/vxml/controllers/ReservationDialogController.java
@@ -9,14 +9,11 @@ import javax.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import pl.com.imralav.vxml.entities.Booking;
 import pl.com.imralav.vxml.entities.Customer;
