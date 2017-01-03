14d37282a73bef5e833e4e596ed09c47f7890f8e
Tomasz Karczewski <tomasz.karczewski@contractors.roche.com>
2017-01-03 10:11:39 +0100

    Issue#21 added movies displaying

--- frontend/webapp/js/viewer/controller.js
+++ frontend/webapp/js/viewer/controller.js
@@ -1,11 +1,18 @@
 angular.module('CinemaReservations')
-  .controller('ViewerController', ['$scope', 'CustomerService', function($scope, customerService) {
+  .controller('ViewerController', ['$scope', 'CustomerService', 'MovieService', function($scope, customerService, moviesService) {
 	  'use strict';
 	  function refreshCustomers() {
 		  customerService.getCustomers(function(customers) {
 			  $scope.customers = customers;
 		  });
 	  }
+	  function refreshMovies() {
+		  moviesService.getMovies(function(movies) {
+			  $scope.movies = movies;
+		  });
+	  }
 	  refreshCustomers();
+	  refreshMovies();
 	  $scope.refreshCustomers = refreshCustomers;
+	  $scope.refreshMovies = refreshMovies;
   }]);
